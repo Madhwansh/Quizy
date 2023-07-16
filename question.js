5ab76c939ccb66a36e3e52447aa31ade174fa5ba
Madhwansh <108359887+Madhwansh@users.noreply.github.com>
2023-07-16 16:13:43 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,47 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C.Hyper Text Markup Language",
+    options: [
+      "A.Hyper Type Multiple Language",
+      "B.Hexa Text Making Language",
+      "C.Hyper Text Markup Language",
+      "D.Home Text Markup Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A.Cascading Style Sheet",
+    options: [
+      "A.Cascading Style Sheet",
+      "B.Capture Style Sheet",
+      "C.Coding Stick",
+      "D.Computer Super Speed",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does Javascript add to the website?",
+    answer: "B.Responsiveness",
+    options: ["A.Structure", "B.Responsiveness", "C.Styling", "D.Animation"],
+  },
+  {
+    numb: 4,
+    question: "Which is one of the popular front end Framework?",
+    answer: "A.React",
+    options: ["A.React", "B.Spring", "C.Node", "D.RubyonRails"],
+  },
+  {
+    numb: 5,
+    question: "What does PHP stand for?",
+    answer: "B.Hypertext Preprocessor",
+    options: [
+      "A.Hometext Programming",
+      "B.Hypertext Preprocessor",
+      "C.Hypertext Preprogramming",
+      "D.Prgramming Hypertext Processor",
+    ],
+  },
+];
